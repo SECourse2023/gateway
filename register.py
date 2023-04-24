65bcfc2ad902808675b6c3175fcddbcaebfc0ce7
lhrrhl0419 <lhrrhl0419@163.com>
2023-04-24 19:34:01 +0800

    organize code

--- register.py
+++ register.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import os
+import warnings
 
 publicKey = os.environ['DOLINK_PUBLIC_KEY']
 privateKey = os.environ['DOLINK_PRIVATE_KEY']
 do_url = 'http://39.104.200.8:18010'
 
-def read_request(api_url):
+def get_request(api_url):
     # Make the API call to receive the message
     response = requests.get(api_url)
 
@@ -19,17 +20,7 @@ def read_request(api_url):
     else:
         print(f"Error: {response.status_code}")
 
-def read_restful(api_url):
-
-    return read_request(api_url)
-
-def read_doip(api_url):
-
-    result = read_request(api_url)
-    assert result['operationId'] == '0.DOIP/Op.Search'
-    return result['attributes']
-
-def write_restful_api(api_url, message):
+def post_request(api_url, message):
     # Make the API call to receive the message
     response = requests.post(api_url, json=message)
 
@@ -42,8 +33,19 @@ def write_restful_api(api_url, message):
     else:
         print(f"Error: {response.status_code}")
 
+
+def read_restful(api_url):
+    return get_request(api_url)
+
+def read_doip(api_url):
+    result = get_request(api_url)
+    assert result['operationId'] == '0.DOIP/Op.Search'
+    return result['attributes']
+
+
 def register_do(message=None):
     if message is None:
+        warnings.warn('no message provided, using default message', UserWarning)
         message = dict(test_key='test_value')
 
     arg = dict(handleValues=message ,sm2KeyPair=dict(publicKey=publicKey, privateKey=privateKey))
@@ -52,30 +54,18 @@ def register_do(message=None):
             '/BDO?action=callBDO&shortId=GlobalRouter&operation=registerByHTTP&' +\
             f'arg={json.dumps(arg)}'
 
-    response = requests.get(api_url)
-
-    # Check if the API call was successful
-    if response.status_code == 200:
-        # Print the returned value
-        message = json.loads(response.content)
-        assert message['status'] == 'Success'
-        return message['result']
-    else:
-        print(f"Error: {response.status_code}")
+    message = get_request(api_url)
+    assert message['status'] == 'Success'
+    return message['result']
 
 def resolve_do(doid):
     api_url = do_url +\
              '/BDO?action=callBDO&shortId=GlobalRouter&operation=resolveByHTTP&arg=' +\
              doid.replace('/', '%2F')
 
-    response = requests.get(api_url)
-
-    if response.status_code == 200:
-        message = json.loads(response.content)
-        assert message['status'] == 'Success'
-        return message['result']
-    else:
-        print(f"Error: {response.status_code}")
+    message = get_request(api_url)
+    assert message['status'] == 'Success'
+    return message['result']
 
 if __name__ == '__main__':
     register_id = register_do()
